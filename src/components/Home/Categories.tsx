8d79265adc27be19b9a0ccb54faa88ec6c9626b7
ferdousalam007 <iamferdousalam007@gmail.com>
2024-07-25 15:08:30 +0600

    category section caruousal card design

--- src/components/Home/Categories.tsx
+++ src/components/Home/Categories.tsx
@@ -1,4 +1,4 @@
-import SectionHeading from "@/components/SectionHeading"
+import SectionHeading from "@/components/SectionHeading";
 import { Card, CardContent } from "@/components/ui/card";
 import Autoplay from "embla-carousel-autoplay";
 import {
@@ -8,14 +8,22 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {
+  // Card,
+  // CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Categories = () => {
   const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
+    Autoplay({ delay: 40000000, stopOnInteraction: true })
   );
   return (
-    <div className="container">
+    <div className="container mb-20">
       <SectionHeading
         headingText="Categories"
         paragraphText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium"
@@ -23,33 +31,52 @@ const Categories = () => {
       <div>
         <Carousel
           opts={{
-            align: "start",
+            align: "end",
             loop: true,
           }}
           plugins={[plugin.current]}
-          className="container p-0"
+          className="container pt-8"
         >
           <CarouselContent>
             {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4 ">
                 <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-3xl font-semibold">
-                        {index + 1}
-                      </span>
+                  <Card className="border-0">
+                    <CardContent className="flex flex-col items-center px-3 py-4 ">
+                      <div className="h-[250px] w-[250px]  bg-white rounded-full">
+                        <img
+                          src="https://vitasana.qodeinteractive.com/wp-content/uploads/2024/03/HOME-3-PRODUCT-06.jpg"
+                          // src={image}
+                          // alt={title}
+                          className="w-full h-full  object-cover  rounded-full p-4 border-[20px]"
+                        />
+                      </div>
                     </CardContent>
+                    <div className="bg-[#F9EFE5] rounded-lg pb-4 pt-[150px] mt-[-150px]">
+                      <h3 className="mt-4 text-center text-xl font-medium">
+                        {/* {title} */}name
+                      </h3>
+                      <div className=" mt-2 text-center">Total Products:50</div>
+
+                      <CardFooter className="flex justify-center p-4">
+                        <Button className="mt-4">
+                          {/* <ShoppingCartIcon className="w-5 h-5 mr-2 text-gray-100" /> */}
+                          {/* <Link to={`/products/${id}`}>View Details</Link> */}
+                          deatail
+                        </Button>
+                      </CardFooter>
+                    </div>
                   </Card>
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
+          <CarouselPrevious className="absolute top-[110%]  left-[45%]" />
+          <CarouselNext className="absolute top-[110%]    right-[45%]" />
         </Carousel>
       </div>
     </div>
   );
-}
+};
 
-export default Categories
+export default Categories;
